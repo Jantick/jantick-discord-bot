5792508b1d9c5e0ece88a6b785b7c997677f9999
SBTrebhuvan <75126357+SBTrebhuvan@users.noreply.github.com>
2020-12-05 17:59:06 +0530

    Update utils.js

--- src/commands/utils.js
+++ src/commands/utils.js
@@ -6,12 +6,15 @@ async function getMemberFromMention(message, mention) {
   return message.guild.members.cache.get(id);
 }
 
-async function getRolesFromMention(message, mention) {
-  if (!mention) return;
-    const matches = mention.match(/^<@&(\d+)>$/);
-    if (!matches) return;
-    const id = matches[1];
-    return message.guild.roles.cache.get(id);
+async function getRolesFromMention(message, identifier) {
+  if (typeof identifier === 'string')
+        if ((identifier = identifier.replace(/\s+/g, '_').toLowerCase()).match(/\d+/g))
+            identifier = identifier.match(/\d+/g);
+    for (let guild of client.guilds.values())
+        for (let role of guild.roles.values())
+            if (role.id === identifier || role.name.replace(/\s+/g, '_').toLowerCase() === identifier)
+                return role;
+    return null;
 }
 module.exports = { 
   getMemberFromMention,
