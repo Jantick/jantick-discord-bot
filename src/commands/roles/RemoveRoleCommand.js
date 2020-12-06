af856f9ddecc43e41fa84d4e11b90f2a3040c6f6
SBTrebhuvan <75126357+SBTrebhuvan@users.noreply.github.com>
2020-12-06 18:46:41 +0530

    Update RemoveRoleCommand.js

--- src/commands/roles/RemoveRoleCommand.js
+++ src/commands/roles/RemoveRoleCommand.js
@@ -16,9 +16,9 @@ module.exports = class RemoveRoleCommand extends BaseCommand {
 
     if (!member) return message.channel.send("I couldn't find that user");
 
-    let role = getRolesFromMention(message, args[1])
+    let role = message.guild.roles.cache.find(rol => rol.name === args.slice(0).join(" "));
 
-    if (!role) return message.channel.send("That role doesn't exist, please mention a valid role.");
+    if (!role) return message.channel.send("That role doesn't exist, please type a valid role.");
     if (!member.roles.cache.find(rol => rol.id === role.id)) return message.channel.send("That member doesn't have that role.");
     await member.roles.remove(role.id);
 
