430504e8b36097fdb22695cd364b0ae61431f94a
SBTrebhuvan <75126357+SBTrebhuvan@users.noreply.github.com>
2020-12-06 15:35:23 +0530

    Create guildMemberAdd.js

---
+++ src/events/guildMemberAdd/guildMemberAdd.js
@@ -0,0 +1,29 @@
+
+const { Message, DiscordAPIError } = require('discord.js');
+const BaseEvent = require('../../utils/structures/BaseEvent');
+const { MessageEmbed } = require('discord.js')
+
+module.exports = class GuildMemberAddEvent extends BaseEvent {
+  constructor() {
+    super('guildMemberAdd');
+  }
+  
+  async run(client, member) {
+    guildMember.addRole(ember.guild.roles.find(role => role.name === "member"));
+
+    let WelcomeEmbed = new MessageEmbed()
+    .setDescription(`${member} *[${member.id}]* has joined the Jantick Technologies Discord.`)
+    .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+    .addField('User', member, true)
+    .addField('Discriminator', `\`#${member.user.discriminator}\``, true)
+    .addField('ID', `\`${member.id}\``, true)
+    .addField('Joined server on', `\`${moment(member.joinedAt).format('MMM DD YYYY')}\``, true)
+    .addField('Joined Discord on', `\`${moment(member.user.createdAt).format('MMM DD YYYY')}\``, true)
+    .setColor('#4BCC85')
+
+    let channel = message.guild.channels.find(ch => ch.id === '778870460879994920')
+    
+    channel.send(WelcomeEmbed)  
+
+  } 
+}
