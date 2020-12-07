1f4ce1e510dccb164f47b67e05019eaa7eb1bea5
MrVarsp <65226823+DeveloperVarsp@users.noreply.github.com>
2020-12-07 17:57:54 +1100

    Update ClearCommand.js

--- src/commands/mod/ClearCommand.js
+++ src/commands/mod/ClearCommand.js
@@ -8,6 +8,8 @@ module.exports = class ClearCommand extends BaseCommand {
   async run(client, message) {
     message.delete()
 
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("This command is restriced to Jantick Staff.");
+
     const args = message.content.split(' ').slice(1); 
     const amount = args.join(' '); 
     
