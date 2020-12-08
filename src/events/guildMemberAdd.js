435deb03fa670bcc5ad10b2cc01e10595379753b
blake <65226823+blakealford@users.noreply.github.com>
2020-12-08 20:17:30 +1100

    Update guildMemberAdd.js

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -9,21 +9,21 @@ module.exports = class GuildMemberAddEvent extends BaseEvent {
   }
   
   async run(client, member) {
-    guildMember.addRole(ember.guild.roles.find(role => role.name === "member"));
+//     guildMember.addRole(ember.guild.roles.find(role => role.name === "member"));
 
-    let WelcomeEmbed = new MessageEmbed()
-    .setDescription(`${member} *[${member.id}]* has joined the Jantick Technologies Discord.`)
-    .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-    .addField('User', member, true)
-    .addField('Discriminator', `\`#${member.user.discriminator}\``, true)
-    .addField('ID', `\`${member.id}\``, true)
-    .addField('Joined server on', `\`${moment(member.joinedAt).format('MMM DD YYYY')}\``, true)
-    .addField('Joined Discord on', `\`${moment(member.user.createdAt).format('MMM DD YYYY')}\``, true)
-    .setColor('#4BCC85')
+//     let WelcomeEmbed = new MessageEmbed()
+//     .setDescription(`${member} *[${member.id}]* has joined the Jantick Technologies Discord.`)
+//     .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+//     .addField('User', member, true)
+//     .addField('Discriminator', `\`#${member.user.discriminator}\``, true)
+//     .addField('ID', `\`${member.id}\``, true)
+//     .addField('Joined server on', `\`${moment(member.joinedAt).format('MMM DD YYYY')}\``, true)
+//     .addField('Joined Discord on', `\`${moment(member.user.createdAt).format('MMM DD YYYY')}\``, true)
+//     .setColor('#4BCC85')
 
-    let Channel = message.guild.channels.find(ch => ch.id === '778870460879994920')
+//     let Channel = message.guild.channels.find(ch => ch.id === '778870460879994920')
     
-    Channel.send(WelcomeEmbed)  
+//     Channel.send(WelcomeEmbed)  
 
   } 
 }
